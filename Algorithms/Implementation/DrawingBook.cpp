081687082b8e0b10e7cdcbdd00a0e7c299ec80ce
MonicaYam <45007263+MonicaYam@users.noreply.github.com>
2022-03-29 15:51:13 -0400

    Create DrawingBook.cpp

---
+++ Algorithms/Implementation/DrawingBook.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+/*
+ * Complete the 'pageCount' function below.
+ *
+ * The function is expected to return an INTEGER.
+ * The function accepts following parameters:
+ *  1. INTEGER n
+ *  2. INTEGER p
+ */
+
+int pageCount(int n, int p) {
+  // p/2 : turn a page from the first
+  // n/2 - p/2 :  turn a page from the last.
+  return min(p/2, (n/2)-(p/2));
+}
+
+int main()
+{
+    int n = 6, p = 2;
+
+    int result = pageCount(n, p);
+
+    cout << result << endl;
+    return 0;
+}
+
